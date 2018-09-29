baaccb130500f476f270b3153de16ca5c79c8072
Kartikeya P. Malimath <kartikeyapm@gmail.com>
2018-09-29 15:10:54 +0530

    index.js for heroku

--- index.js
+++ index.js
@@ -31,20 +31,20 @@ restService.use(bodyParser.json());
 restService.post("/number", (req, res) => {
     const userdb = new User();
     
-    userdb.phone_number = req.body.phone_number;
-        //req.body.result &&
-        //req.body.result.parameters &&
-        //req.body.result.parameters.phone_number;
+    userdb.phone_number = //req.body.phone_number;
+        req.body.result &&
+        req.body.result.parameters &&
+        req.body.result.parameters.phone_number;
 
-    userdb.userName = req.body.userName;
-        //req.body.result &&
-        //req.body.result.parameters &&
-        //req.body.result.parameters.name;
+    userdb.userName = //req.body.userName;
+        req.body.result &&
+        req.body.result.parameters &&
+        req.body.result.parameters.name;
 
-    userdb.age = req.body.age;
-        //req.body.result &&
-        //req.body.result.parameters &&
-        //req.body.result.parameters.age;
+    userdb.age = //req.body.age;
+        req.body.result &&
+        req.body.result.parameters &&
+        req.body.result.parameters.age;
 
     //User.findone({number : number}).then(user =>)
     userdb.save().then(userSaved => {
